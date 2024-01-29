9e63930fae68f0438749a9821a0afab5f638f714
Mudit_Khanna <Muditkhannatech@gmail.com>
2024-01-29 08:38:52 +0530

    added functionalities like sign out, and sign up with google

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import {getAuth,createUserWithEmailAndPassword} from "firebase/auth"
+import { useEffect,useState } from "react";
+import {getAuth,onAuthStateChanged,signOut} from "firebase/auth"
 import { app } from "./firebase";
 import './App.css';
 import SignupPage from "./pages/Signup";
 import SignInPage from "./pages/SignIn";
+
+
 const auth=getAuth(app);
+
 function App() {
 
-  return (
+  const [user,setUser]=useState(null);
+
+ useEffect(()=>{
+  onAuthStateChanged(auth,user=>{
+    if(user){
+      //user is logged in
+      setUser(user)
+    }else{
+      console.log("user is logged out");
+      setUser(null);
+    }
+  })
+ },[])
+ if(user==null){
+ return (
     <div className="App">
      <SignupPage/>
      <SignInPage/>
     </div>
   );
+ }else{
+  return (
+   <div className="App">
+    <h1>Hello there {user.email}</h1>
+    <button onClick={()=>signOut(auth)}>Log out</button>
+   </div>
+  )
+ }
+  
 }
 
 export default App;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import { React,useContext,createContext } from "react";
 import { useState } from "react";
-import { getAuth ,createUserWithEmailAndPassword} from "firebase/auth";
+import { getAuth ,
+    createUserWithEmailAndPassword,
+    GoogleAuthProvider,
+signInWithPopup} from "firebase/auth";
 import {app} from "../firebase"
 
 const auth=getAuth(app);
+const googleProvider= new GoogleAuthProvider();
+
+
 
 const SignupPage=()=>{
     const[email,setemail]=useState("");
@@ -13,6 +19,10 @@ const SignupPage=()=>{
      createUserWithEmailAndPassword(auth,email,password)
      .then((value)=>alert("done"))
     }
+
+    const signupwithGoogle=()=>{
+        signInWithPopup(auth,googleProvider);
+    } 
     return(
         <div className="signup-page">
             <h1>Sign Up page</h1>
@@ -20,7 +30,10 @@ const SignupPage=()=>{
             <input onChange={(e)=>setemail(e.target.value)} value={email} type="email" required placeholder="Enter your email here"/>
             <label >Password</label>
             <input onChange={(e)=>setpassword(e.target.value)} value={password} type="password" required placeholder="password" />
+            <br/>
+            <button onClick={signupwithGoogle}>Sign in With Google</button>
             <button onClick={createUser}>Signup</button>
+       
         </div>
     )
 }
